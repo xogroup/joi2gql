f6e15a8716622c41c2eee3e636bcce9345bbb713
Samueljoli <samuel.joli.ftn@gmail.com>
2017-08-28 12:01:15 -0400

    extend .composeSchema to allow for schema generation given a joi schema

--- src/methods/composeSchema.js
+++ src/methods/composeSchema.js
@@ -5,15 +5,24 @@ const {
     GraphQLSchema
 } = require('graphql');
 
+const { typeDictionary } = require('../helpers');
+
 const buildFields = (obj) => {
     const attrs = {};
 
     for (let key in obj) {
-        attrs[key] = {
-            type   : obj[key],
-            args   : obj[key]._typeConfig.args,
-            resolve: obj[key]._typeConfig.resolve
-        };
+        if (obj[key].isJoi) {
+            attrs[key] = {
+                type   : typeDictionary[obj[key]._type],
+                resolve: obj[key]._meta.find((item) => item.resolve instanceof Function).resolve
+            };
+        } else {
+            attrs[key] = {
+                type   : obj[key],
+                args   : obj[key]._typeConfig.args,
+                resolve: obj[key]._typeConfig.resolve
+            };
+        }
     }
 
     return attrs;
